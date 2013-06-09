193aec9993b368dea198a168f07ca5d3b2aa459f
trinityhaxxor <trinityhaxxor@gmail.com>
2013-06-09 23:00:03 +0300

    Added mpdecision V1

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 103 */
+/* This file is auto generated, version 105 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#103 SMP PREEMPT Sun Jun 9 20:20:28 AST 2013"
+#define UTS_VERSION "#105 SMP PREEMPT Sun Jun 9 22:56:16 AST 2013"
 #define LINUX_COMPILE_BY "TrinityHaxxorX"
 #define LINUX_COMPILE_HOST "XEON-IDE#1"
-#define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0HaxxorX"
+#define UTS_RELEASE "3.4.0HaxxorX-gd0fdbab-dirty"
